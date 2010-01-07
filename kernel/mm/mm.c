5180110d9e5045e8fd8579d52b0be8236df6a7e1
Masami Ichikawa <masami256@gmail.com>
2010-01-08 01:13:20 +0900

    Refactoring has done.

--- kernel/mm/mm.c
+++ kernel/mm/mm.c
@@ -5,49 +5,22 @@
 
 static void page_fault(struct registers regs);
 
-// This variable should be aligned to page boundary.
-//pgd_t pgd[1024] __attribute__((aligned(0x1000)));
-extern pgd_t pgd;
-
-pgd_t *page_directory;
-pte_t *pte;
+// This should be page aligned.
+pgd_t page_directory[1024] __attribute__((aligned(0x1000))); 
+pte_t pte[1024];
 
 static void setup_pte(void)
 {
-#if 0
-	int i;
-	pte = (pte_t *) &pgd;
-	u_int32_t addr = 0;
-
-	for (i = 0; i < sizeof(pgd) / sizeof(pgd[0]); i++) {
-		pte[i] = addr | PAGE_PRESENT | PAGE_RW;
-		addr += 0x1000;
-	}
-#else
-	unsigned int page_aligned_end = (pgd & 0xfffff000) + 0x1000;
-	int i = 0;
 	unsigned int address = 0; 
+	int i;
 
-	page_directory = (pgd_t *) page_aligned_end;
+	page_directory[0] = (pgd_t) &pte | PAGE_PRESENT | PAGE_RW;
 
 	for(i = 0; i < 1024; i++) {
-		//attribute: supervisor level, read/write, not present.
-		page_directory[i] = 0 | 2; 
+		// PAGE is present and readable/writable.
+		pte[i] = address | PAGE_PRESENT | PAGE_RW; 
+		address = address + 4096; 
 	}
-	
-	pte = page_directory + 0x1000;
-	
-        //we will fill all 1024 entries, mapping 4 megabytes
-	for(i = 0; i < 1024; i++) {
-		pte[i] = address | 3; // attributes: supervisor level, read/write, present.
-		address = address + 4096; //advance the address to the next page boundary
-	}
-
-	page_directory[0] = pte; 
-	page_directory[0] |= 3;// attributes: supervisor level, read/write, present
-#endif
-
-
 }
 
 static void set_cr3(void)
@@ -78,7 +51,6 @@ void setup_paging(void)
 	set_cr3();
 
 	printk("set cr0: enable paging\n");
-
 	set_cr0();
 }
 
@@ -88,4 +60,5 @@ void setup_paging(void)
 static void page_fault(struct registers regs)
 {
 	printk("Page Fault\n");
+	while (1);
 }
