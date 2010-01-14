8e9e6c3e90a69dad344a2c2e6a674ee697c2fe31
Masami Ichikawa <masami256@gmail.com>
2010-01-15 00:53:27 +0900

    Added

--- kernel/mm/mm.c
+++ kernel/mm/mm.c
@@ -6,8 +6,8 @@
 static void page_fault(struct registers regs);
 
 // This should be page aligned.
-pgd_t page_directory[1024] __attribute__((aligned(0x1000))); 
-pte_t pte[1024];
+pgd_t page_directory[PGD_SIZE] __attribute__((aligned(0x1000))); 
+pte_t pte[PTE_SIZE];
 
 /////////////////////////////////////////////////
 // private functions
@@ -22,11 +22,14 @@ static void setup_pte(void)
 
 	page_directory[0] = (pgd_t) &pte | PAGE_PRESENT | PAGE_RW;
 
-	for(i = 0; i < 1024; i++) {
+	for(i = 0; i < PTE_SIZE; i++) {
 		// PAGE is present and readable/writable.
 		pte[i] = address | PAGE_PRESENT | PAGE_RW; 
-		address = address + 4096; 
+		address = address + PAGE_SIZE; 
 	}
+
+	// Address 0 is reserved.
+	pte[0] |= PAGE_ACCESS;
 }
 
 /**
@@ -58,9 +61,65 @@ static void page_fault(struct registers regs)
 	while (1);
 }
 
+/**
+ * Check access bit.
+ * @param index of pte.
+ * @return if haven't accessed yet it returns 0.
+ */
+static int is_free_page(u_int32_t idx)
+{
+	return pte[idx] & PAGE_ACCESS;
+}
+
+/**
+ * Clear access bit.
+ * @param index of pte.
+ */
+static void clear_access_bit(u_int32_t idx)
+{
+	pte[idx] &= ~PAGE_ACCESS;
+}
+
 /////////////////////////////////////////////////
 // public functions
 /////////////////////////////////////////////////
+
+/**
+ * Find free page(s).
+ * @param  how many pages?
+ * @return if there is no page it returns 0. if there is enough page, it returns first page's address.
+ */
+u_int32_t get_free_pages(u_int32_t pages)
+{
+	// current index for pte.
+	static u_int32_t current_idx = 0;
+	u_int32_t prev;
+	u_int32_t i;
+	u_int32_t enough;
+	u_int32_t ret = 0;
+
+	enough = 0;
+	for (i = 0; i < PAGE_SIZE; i++) {
+		if (current_idx == PTE_SIZE)
+			current_idx = 0;
+
+		if (!enough)
+			ret = pte[current_idx] & 0xfffff000;
+
+		if (!is_free_page(current_idx)) {
+			enough++;
+			clear_access_bit(current_idx);
+			if (enough == pages) {
+				current_idx++;
+				break;
+			}
+		} else {
+			enough = 0;
+		}
+		current_idx++;
+	} 
+	return ret;
+}
 
 /**
  * Setup PGD, PTE then enable paging.
@@ -68,6 +127,7 @@ static void page_fault(struct registers regs)
 void setup_paging(void)
 {
 	printk("Setup paging.\n");
+	int i;
 
 	setup_pte();
 
@@ -76,5 +136,6 @@ void setup_paging(void)
 	set_cr3();
 
 	set_cr0();
+
 }
 
--- kernel/mm/mm.h
+++ kernel/mm/mm.h
@@ -10,6 +10,10 @@ typedef u_int32_t pte_t;
 #define PAGE_ACCESS  0x20 // Access bit
 #define PAGE_DIRTY   0x40 // Dirty bit
 
+#define PAGE_SIZE    0x1000 // 4KB page
+#define PGD_SIZE     1024
+#define PTE_SIZE     1024
+
 void setup_paging(void);
 
 #endif // __MM_H
