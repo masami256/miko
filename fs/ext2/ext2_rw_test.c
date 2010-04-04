ff9e2cbaf01db3ce3ab2477ce38f5474d27b90a4
masami256 <masami256@gmail.com>
2010-04-04 22:20:11 +0900

    Added include files.

--- fs/ext2/ext2_rw_test.c
+++ fs/ext2/ext2_rw_test.c
@@ -1,6 +1,10 @@
 #include <mikoOS/kernel.h>
 #include <mikoOS/printk.h>
+
 #include "ext2fs.h"
+#include "ext2_inode.h"
+#include "ext2_blockgroup.h"
+#include "ext2_dentry.h"
 
 int test_ext2_read(void)
 {
