85bc3db07473e26171f5fd9c6da14f5075162f96
masami256 <masami256@gmail.com>
2010-05-02 14:59:35 +0900

    Print  superblock information.

--- fs/minix/minixfs.c
+++ fs/minix/minixfs.c
@@ -27,12 +27,22 @@ static int minix_get_sb(struct vfs_mount *vmount)
 	printk("%s\n", __FUNCTION__);
 
 	ret = read_one_block(vmount->driver, &sblock);
+	memcpy(&minix_sb, sblock.data, sizeof(minix_sb));
 
-	{
-		int i;
-		for (i = 0; i < 32; i++)
-			printk("0x%x ", sblock.data[i] & 0xff);
-	}
+	printk(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+	printk("Superblock info\n");
+	printk("s_ninodes: 0x%x\n", minix_sb.s_ninodes);
+	printk("s_nzones:  0x%x\n", minix_sb.s_nzones);
+	printk("s_imap_blocks: 0x%x\n", minix_sb.s_imap_blocks);
+	printk("s_zmap_blocks: 0x%x\n", minix_sb.s_zmap_blocks);
+	printk("s_firstdatazone: 0x%x\n", minix_sb.s_firstdatazone);
+	printk("s_log_zone_size: 0x%x\n", minix_sb.s_log_zone_size);
+	printk("s_max_size: 0x%x\n", minix_sb.s_max_size);
+	printk("s_magic: 0x%x\n", minix_sb.s_magic);
+	printk("s_pad: 0x%x\n", minix_sb.s_pad);
+	printk("s_zones: 0x%x\n", minix_sb.s_zones);
+	printk(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n");
+
 	return 0;
 }
 
